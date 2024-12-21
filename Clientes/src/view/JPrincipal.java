f04fa81dc7888a206a8575cea89a052b0a8a46e4
júlia <162610003+juliamacedo63@users.noreply.github.com>
2024-12-21 09:23:38 -0300

    Update JPrincipal.java

--- Clientes/src/view/JPrincipal.java
+++ Clientes/src/view/JPrincipal.java
@@ -40,7 +40,7 @@ public class JPrincipal extends JFrame {
 	 */
 	public JPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 873, 555);
+		setBounds(100, 100, 1434, 881);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(128, 128, 192));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -50,7 +50,7 @@ public class JPrincipal extends JFrame {
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(88, 82, 160));
-		panel.setBounds(83, 26, 696, 456);
+		panel.setBounds(231, 91, 1378, 820);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
@@ -60,7 +60,7 @@ public class JPrincipal extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnJogoDaVelha.setBounds(373, 132, 273, 252);
+		btnJogoDaVelha.setBounds(839, 257, 273, 252);
 		panel.add(btnJogoDaVelha);
 		
 		JButton btnNewButton = new JButton("New button");
@@ -69,38 +69,47 @@ public class JPrincipal extends JFrame {
 			}
 		});
 		btnNewButton.setIcon(new ImageIcon("D:\\Aluno\\Imagens\\sticker-png-math-calculator-computer-icons-squaremath-information-calculadora-service-logo-calculator-area-sales.png"));
-		btnNewButton.setBounds(63, 144, 221, 197);
+		btnNewButton.setBounds(225, 257, 221, 197);
 		panel.add(btnNewButton);
 		
 		JLabel lblNewLabel = new JLabel("CALCULADORA");
 		lblNewLabel.setForeground(new Color(255, 255, 255));
 		lblNewLabel.setBackground(new Color(255, 255, 255));
 		lblNewLabel.setFont(new Font("Yu Gothic UI", Font.BOLD, 23));
-		lblNewLabel.setBounds(91, 80, 181, 41);
+		lblNewLabel.setBounds(251, 191, 202, 58);
 		panel.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("JOGO DA  VELHA");
 		lblNewLabel_1.setFont(new Font("Yu Gothic UI", Font.BOLD, 23));
 		lblNewLabel_1.setForeground(new Color(255, 255, 255));
-		lblNewLabel_1.setBounds(414, 89, 201, 22);
+		lblNewLabel_1.setBounds(867, 209, 201, 22);
 		panel.add(lblNewLabel_1);
 		
-		JButton btnNewButton_1 = new JButton("VOLTAR PARA A PÁGINA DE LOGIN");
-		btnNewButton_1.setEnabled(false);
-		btnNewButton_1.setForeground(new Color(0, 0, 0));
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton_1.setBackground(new Color(224, 54, 88));
-		btnNewButton_1.setBounds(231, 406, 230, 23);
-		panel.add(btnNewButton_1);
-		
 		JLabel lblNewLabel_2 = new JLabel("ESCOLHA");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_2.setForeground(new Color(255, 255, 255));
 		lblNewLabel_2.setFont(new Font("Yu Gothic UI", Font.PLAIN, 34));
-		lblNewLabel_2.setBounds(252, 28, 144, 41);
+		lblNewLabel_2.setBounds(640, 76, 144, 41);
 		panel.add(lblNewLabel_2);
+		
+		JButton btnNewButton_1 = new JButton("VOLTAR PARA A PÁGINA DE LOGIN");
+		btnNewButton_1.setBounds(543, 518, 230, 23);
+		panel.add(btnNewButton_1);
+		btnNewButton_1.setForeground(new Color(0, 0, 0));
+		btnNewButton_1.addActionListener(new ActionListener() {
+			
+		public void actionPerformed(ActionEvent e) {
+				btnNewButton_1.addActionListener(new ActionListener() {
+				    public void actionPerformed(ActionEvent e) {
+				        JLogin telaLogin = new JLogin();  
+				        telaLogin.setVisible(true);        
+				        dispose();                      
+				    }
+				});
+
+			}
+		});
+		btnNewButton_1.setBackground(new Color(192, 192, 192));
 	}
 
 }
